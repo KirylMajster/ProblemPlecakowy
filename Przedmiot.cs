87cc0a6963dd2c32a4009f39b86802923c861627
Antoni Kirylczuk <antekkirylczuk@gmail.com>
2025-03-31 21:46:46 +0200

    Dodaj pliki projektów.

---
+++ Przedmiot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProblemPlecakowy
+{
+	public class Przedmiot
+	{
+		public int Waga { get; }
+		public int Wartosc { get; }
+		public Przedmiot(int waga, int wartosc)
+		{
+			Waga = waga;
+			Wartosc = wartosc;
+		}
+
+		public override string ToString()
+		{
+			return $"Przedmiot - Waga: {Waga}, Wartosc: {Wartosc}";
+		}
+	}
+}
